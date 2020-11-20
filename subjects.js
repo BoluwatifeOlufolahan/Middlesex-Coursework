7c437ef91496be9eeecd17e3d18140b3ac185c55
BoluwatifeOlufolahan <73609662+BoluwatifeOlufolahan@users.noreply.github.com>
2020-11-20 16:03:48 +0100

    Initialize dev

---
+++ subjects.js
@@ -0,0 +1,82 @@
+let subjects = [
+    {
+        id: 1,
+        title: "Geography",
+        location: "London",
+        price: 10000,
+        image: "images/geography.png",
+        spaces: 5
+    },
+    {
+        id: 2,
+        title: "Chemistry",
+        location: "Oxford",
+        price: 10000,
+        image: "images/chemistry.png",
+        spaces: 5
+    },
+    {
+        id: 3,
+        title: "English",
+        location: "Nigeria",
+        price: 10000,
+        image: "images/english.png",
+        spaces: 5
+    },
+    {
+        id: 4,
+        title: "Literature",
+        location: "York",
+        price: 8000,
+        image: "images/literature.png",
+        spaces: 5
+    },
+    {
+        id: 5,
+        title: "Music",
+        location: "Bristol",
+        price: 9000,
+        image: "images/music.png",
+        spaces: 5
+    },
+    {
+        id: 6,
+        title: "Biology",
+        location: "London",
+        price: 10000,
+        image: "images/biology.png",
+        spaces: 5
+    },
+    {
+        id: 7,
+        title: "Chemistry",
+        location: "Oxford",
+        price: 10000,
+        image: "images/chemistry.png",
+        spaces: 5
+    },
+    {
+        id: 8,
+        title: "History",
+        location: "London",
+        price: 10000,
+        image: "images/history.png",
+        spaces: 5
+    },
+    {
+        id: 9,
+        title: "Algebra",
+        location: "York",
+        price: 8000,
+        image: "images/algebra.png",
+        spaces: 5
+    },
+    {
+        id: 10,
+        title: "Physical Education",
+        location: "Bristol",
+        price: 9000,
+        image: "images/physical education.png",
+        spaces: 5
+    }
+]
